e19a1d5bd019c45cfad25ddd02bd43d87af8fbe5
hashimkalam <hashim.20211291@iit.ac.lk>
2024-03-22 00:22:43 +0530

    coded getLLamaRes function

--- app.py
+++ app.py
@@ -3,3 +3,27 @@ from langchain.prompts import PromptTemplate
 from langchain.llms import CTransformers
 
 # function to get the response from the llama2 model
+
+def getLLamaRes():
+
+    # loading the model
+    llm = CTransformers(model="models\llama-2-7b-chat.ggmlv3.q8_0.bin",
+                        model_type="llama",
+                        config={'max_new_tokens': 256,
+                                'temperature': .01})
+    
+
+    # prompt template
+
+    template="""
+    Write a blog for __ job profile for a topic __ within __ words
+    """
+
+
+    prompt=PromptTemplate(input_variables=["style", "text", "n_words"], 
+                          template=template)
+    
+    # generating res from the loaded llama 2 model
+    response = llm(prompt.format(style=blog_style, text=input_text, n_words = no_words))
+    print(response)
+    return response
